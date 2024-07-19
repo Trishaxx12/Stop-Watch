ea1eb6c86a8821de86aa5b4ebfe9bec87b7aebd2
trishadeepak12 <trishasudarsan12@gmail.com>
2024-07-19 09:48:34 +0530

    Final Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import StopWatch from './components/StopWatchComponents/StopWatchComponent';
+
+
+
+const App = () => {
+  return (
+    <div>
+      <StopWatch/>
+      
+    </div>
+  );
+};
+
+export default App;
